f28604d7f7b50ee8a20ad38fcc850515873eae3d
Mitu1994 <mituflower@gmail.com>
2017-12-15 19:13:32 +0530

    commiting

---
+++ jaas-call-back-handler/src/MyCallbackHandler.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+public class MyCallbackHandler implements CallbackHandler{
+
+	@Override
+	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
